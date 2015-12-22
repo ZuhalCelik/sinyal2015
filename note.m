6c8c18c98289606f1796a19ba195693a0c3d2cd1
ZuhalCelik <zuhaalcelik@gmail.com>
2015-12-22 12:32:53 +0200

    sinus sinyali olusturan fonksiyon

--- note.m
+++ note.m
@@ -1,4 +1,9 @@
-function [x,t]=note(frekans,vurus) %frekansi ve vurusu bilinen notanin sin sinyali fonksiyonu tanimlandi
-t=0:1/frekans:vurus-1/frekans; %t aralýgý tanimlandi 
-x=sin(2*pi*frekans*t);   %sinus sinyali olusturma
+function [xx,tt]=note(nfrekans,nsure) %frekansi ve suresi bilinen notanin sinüs sinyalini döndüren fonksiyon tanimlandi.
+Fs=8192;   %örnekleme frekansýna deðer atandý.
+tt=0:1/Fs:((0.25-1/Fs)/2); %t aralýðý tanýmlandý.
+zz=0;                      %zarf matrisinin ilk deðeri atandý.
+   xx=sin(2*pi*nfrekans*tt); %xx,sinüs sinyali fonksiyonu
+if length(xx)==length(zz)    
+       zz=[linspace(0,1.5,Fs*nsure*(2/8)),linspace(1.5,1,Fs*nsure*(1/8)),linspace(1,1,Fs*nsure*(4/8)),linspace(1,0,Fs*nsure*(1/8))];
+end
 end
