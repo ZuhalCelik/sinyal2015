e08ddc75e41b3f9695ebc810e4c520502a70fa45
ZuhalCelik <zuhaalcelik@gmail.com>
2016-01-03 15:07:15 +0200

    sinus sinyali olusturan fonksiyon

--- note.m
+++ note.m
@@ -1,19 +1,22 @@
-function [xx,t]=note(nfrekans,nsure) %frekansi ve suresi bilinen notanin sinüs sinyalini döndüren fonksiyon tanimlandi.
-Fs=8192;                 %örnekleme frekansýna deðer atandý.
-t=0:1/Fs:nsure-1/Fs;     %t aralýðý tanýmlandý.
-harmonik={1 0.8 0.4 0.1};
-a=1;xtop=0;
-   z1=linspace(0,1.5,length(t)/4);
-   z2=linspace(1.5,1,length(t)/8);
-   z3=linspace(1,1,length(t)/2);
-   z4=linspace(1,0,length(t)/8);
-   zarf=[z1 z2 z3 z4]; %zarf olusturuldu.
-   for h=1:length(harmonik)
-       x=harmonik{h}*sin(2*pi*nfrekans*t*a); %harmoniklerin hesaplanmasý. 
-       xtop=xtop+x; %harmonikler toplanarak birlestirildi.
-       a=a+1;
+%% Fonksiyon tanýmý
+function [xx,t]=note(nfrekans,nsure) %frekansi ve suresi bilinen notanin sinüs sinyalini donduren fonksiyon tanimlandi.
+%% Deger atamalarý
+Fs=8192;                   %ornekleme frekansýna deðer atandý.
+t=0:1/Fs:nsure-(1/Fs);     %t aralýgý tanýmlandý.
+harmonik={1 0.8 0.4 0.1};xtop=0;
+%% Zarf paketinin olusturulmasý
+   Zattack=linspace(0,1.5,length(t)/4); %attack suresi:1/4lük (t)surede 0'dan 1.5 katýna kadar arttý.
+   Zdecay=linspace(1.5,1,length(t)/8);  %decay suresi:sonraki 1/8lik (t)surede normal genliðine geri dondu.
+   Zsustain=linspace(1,1,length(t)/2);  %sustain suresi:sonraki 1/2lik sürede normal genliðinde devam etti.
+   Zrelease=linspace(1,0,length(t)/8);  %release suresi:sonraki 1/8lik sürede normal genliðinden azalarak 0'a gitti.
+   zarf=[Zattack Zdecay Zsustain Zrelease]; %zarf paketi olusturuldu.
+%% Harmoniklerin hesaplanmasý
+   for i=1:length(harmonik)
+       x=harmonik{i}*sin(2*pi*nfrekans*t*i); %i. harmonik hesaplandý.
+       xtop=xtop+x;                          %harmonikler toplanarak birbirine eklendi.
    end
-   xx=xtop.*zarf; %notanýn harmonikleri eklenerek zarf ile paketlendi.
+%% Olustulan sinyalin harmonikleri de eklenerek zarf ile paketlenmesi
+   xx=xtop.*zarf; %notanýn harmonikleri eklendi ve zarf ile paketlendi.
 end
 
 
