aea7e32095f5902f189f27dfb768a05209db83cf
ZuhalCelik <zuhaalcelik@gmail.com>
2016-01-03 15:05:51 +0200

    frekansi hesaplayan fonksiyon

--- frek.m
+++ frek.m
@@ -1,22 +1,25 @@
+%% Fonksiyon tanýmý
 function ff=frek(nota,oktav) %girilen nota ve oktav degerine gore frekansi hesaplayan fonksiyon tanimi
+%% Deger atamalarý
 notalar={'Do' 'Dod' 'Re' 'Red' 'Mi' 'Fa' 'Fad' 'Sol' 'Sold' 'La' 'Lad' 'Si' 'sus'};%Lad=La#,Sib;Dod=Do#,Reb;Red=Re#,Mib;Fad=Fa#,Solb;Sold=Sol#,Lab
-referans=16.35;    %formul icin ilk deger atamasi
+referans=16.35;    %referans degeri atandý.
 p=length(notalar); %notalar dizisinin eleman sayisi 
-if nargin<2        %girilen arguman sayýsýnýn 2'den az olma durumu
-    oktav=4;
+%% Oktav degerinin girilmemesi durumu
+if nargin<2        %girilen arguman sayýsýnýn 2'den az olma durumu(oktav degeri girilmemis ise).
+    oktav=4;       %oktav'a 4 degerini ata.
 end
-for okt=0:8        %okt(oktav) degerini 0'dan 8(dahil)'e kadar artirma islemi
+%% Frekans degerinin hesaplanmasý
+for okt=0:8 %okt(oktav) degerini 0'dan 8(dahil)'e kadar artýrma islemi
     if okt==oktav  %girilen oktav degerinin okt degeriyle esitlenmesi halinde
-        for j=1:p  %++dizi eleman indisinin artýrýmý
-            if size(nota)==size(notalar{p}) 
-                if nota==notalar{p}          
-                    ff=2^okt*(2^((j-1)/12)*referans); 
-                end                                 
-            end     
+        for j=1:p  %++dizi eleman indisinin artýrýmý    
             if size(nota)==size(notalar{j}) %girilen ile dizideki notalarin uzunluklari esit ise
                 if nota==notalar{j}         %++girilen notanin dizideki karsiliginin(indisinin) bulunmasi 
-                    ff=2^okt*(2^((j-1)/12)*referans); %oktav ve nota indisine bagli olarak
-                end                                   %++frekansi hesaplayan formul
+                    if j==p                 %++bulunan nota 'sus' ise 
+                        ff=0;               %++frekansý degerini 0 ata.
+                    else                    %'sus' dýsýndaki notalar icin
+                        ff=2^okt*(2^((j-1)/12)*referans); %++oktav ve nota indisine bagli olarak
+                    end                                   %++frekansi hesaplayan formul
+                end                                   
             end    
         end
     end
