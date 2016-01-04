0f3ff8dfb957568aba72b8d503f12304fc2c6bc5
ZuhalCelik <zuhaalcelik@gmail.com>
2016-01-04 23:34:56 +0200

    sesin grafik ve sound.unu verir

--- sentez.m
+++ sentez.m
@@ -1,11 +1,11 @@
 %% Deger atamalarý, dosyadan deger cekme
-Fs=2320; %ornekleme frekansý deger atamasý yapýldý.
+Fs=2323; %ornekleme frekansý deger atamasý yapýldý.
 gecikme=round(Fs/10); %echo icin gecikme suresi tanýmlandý.
 notalar=[]; %notalar matrisine ilk deger atamasý yapýldý.
-duraklama=zeros(1,round(Fs/100)); %Fs/10000; %duraklama suresi degeri atandý.
-oktdegis=0; %oktav degerlerini degistiren degisken[-3,+3].
+duraklama=zeros(1,round(Fs/100)); %duraklama suresi degeri atandý.
+oktdegis=1; %oktav degerlerini degistiren degisken[-3,+3].
 dosya=fopen('notalar.txt','r'); %text dosyasýný acar,'r':okumak için.
-[nota,oktav,olcu]=textread('notalar.txt','%s%d%s','delimiter',','); %text dosyasý okundu ve , ile ayrýlmýs degiskenler yeni matrislere atandý.
+[nota,oktav,olcu]=textread('notalar.txt','%s%d%s','delimiter',','); %text dosyasý okundu ve ',' ile ayrýlmýs degiskenler yeni matrislere atandý.
 fclose(dosya); %text dosyasý kapatýldý.
 %% Program icinde oktav degisimi kosulu
 if oktdegis~=0 %oktdegis degiskeninde degisiklik yapýlmasý durumunda islenecek sart. 
@@ -20,13 +20,13 @@ for i=1:length(nota)
     notalar=[notalar sindalga duraklama]; %notalarýn sinyalleri arasýna bosluk eklenerek notalar matrisinde birlestirildi.
 end  
 %% Notalar matrisine echo ekleme
-% for i=1:length(notalar)
-%     if (i+gecikme)<=length(notalar) %gecikme notalar uzunlugunu asmadýgý surece islenecek sart.
-%         notalar(i+gecikme)=notalar(i+gecikme)+0.3*notalar(i); %Fs, 10'da biri süresince geciktirilip(gecikme)
-%     end                                               %++genliði %30 oranýnda düþürülerek kendisi ile toplandý.
-% end
-%% Notalar matrisi sinyallerinin tepe degeri 1'e normalize edildi.
+for i=1:length(notalar)
+    if (i+gecikme)<=length(notalar) %gecikme notalar uzunlugunu asmadýgý surece islenecek sart.
+        notalar(i+gecikme)=notalar(i+gecikme)+0.3*notalar(i); %Fs, 10'da biri süresince geciktirilip(gecikme)
+    end                                               %++genliði %30 oranýnda düþürülerek kendisi ile toplandý.
+end
+%% Notalar matrisi sinyallerinin tepe degerinin 1'e normalize edilmesi
 notalar=notalar/max(abs(notalar));
-%% Notalar matrisinin grafigi cizdirilip, ses olarak calýndý
+%% Notalar matrisinin grafiginin cizdirilip, ses olarak calýnmasý
 plot(notalar)
 sound(notalar,Fs);
