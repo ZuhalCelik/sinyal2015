078519aa0db5d122606d07b4b0033a1e84900c1d
ZuhalCelik <zuhaalcelik@gmail.com>
2016-01-28 17:16:18 +0200

    frekansi hesaplayan fonksiyon

--- frek.m
+++ frek.m
@@ -9,18 +9,14 @@ if nargin<2        %girilen arguman sayýsýnýn 2'den az olma durumu(oktav degeri 
     oktav=4;       %oktav'a 4 degerini ata.
 end
 %% Frekans degerinin hesaplanmasý
-for okt=0:8 %okt(oktav) degerini 0'dan 8(dahil)'e kadar artýrma islemi
-    if okt==oktav  %girilen oktav degerinin okt degeriyle esitlenmesi halinde
-        for j=1:p  %++dizi eleman indisinin artýrýmý    
-            if size(nota)==size(notalar{j}) %girilen ile dizideki notalarin uzunluklari esit ise
-                if nota==notalar{j}         %++girilen notanin dizideki karsiliginin(indisinin) bulunmasi 
-                    if j==p                 %++bulunan nota 'sus' ise 
-                        ff=0;               %++frekansý degerini 0 ata.
-                    else                    %'sus' dýsýndaki notalar icin
-                        ff=2^okt*(2^((j-1)/12)*referans); %++oktav ve nota indisine bagli olarak
-                    end                                   %++frekansi hesaplayan formul
-                end                                   
-            end    
+for i=1:p  %dizi eleman indisinin artýrýmý    
+    if size(nota)==size(notalar{i}) %girilen ile dizideki notalarin uzunluklari esit ise
+        if nota==notalar{i}         %++girilen notanin dizideki karsiliginin(indisinin) bulunmasi 
+            if i==p                 %++bulunan nota 'sus' ise 
+                ff=0;               %++frekansý degerini 0 ata.
+            else                    %'sus' dýsýndaki notalar icin
+                ff=2^oktav*(2^((i-1)/12)*referans); %++oktav ve nota indisine bagli olarak
+            end                                   %++frekansi hesaplayan formul
         end
     end
 end
