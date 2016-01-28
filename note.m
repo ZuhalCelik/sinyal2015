76285cc96d154743d962adc7db01b4a0ddaf9edc
ZuhalCelik <zuhaalcelik@gmail.com>
2016-01-28 17:17:29 +0200

    sinus sinyali olusturan fonksiyon

--- note.m
+++ note.m
@@ -1,9 +1,9 @@
 %% Fonksiyon tanýmý
 function [xx,t]=note(nfrekans,nsure) %frekansi ve suresi bilinen notanin sinüs sinyalini donduren fonksiyon tanimlandi.
 %% Deger atamalarý
-Fs=8192;                   %ornekleme frekansýna deðer atandý.
-t=0:1/Fs:nsure-(1/Fs);     %t aralýgý tanýmlandý.
-harmonik={1 0.8 0.4 0.1};xtop=0;
+Fs=8192;                   %ornekleme frekansýna deðer atandý.8192
+t=0:1/Fs:nsure-1/Fs;     %t aralýgý tanýmlandý.
+harmonik={1 0.8 0.6 0.4 0.2 0.1};xtop=0;
 %% Zarf paketinin olusturulmasý
    Zattack=linspace(0,1.5,length(t)/4); %attack suresi:1/4lük (t)surede 0'dan 1.5 katýna kadar arttý.
    Zdecay=linspace(1.5,1,length(t)/8);  %decay suresi:sonraki 1/8lik (t)surede normal genliðine geri dondu.
@@ -18,9 +18,3 @@ harmonik={1 0.8 0.4 0.1};xtop=0;
 %% Olustulan sinyalin harmonikleri de eklenerek zarf ile paketlenmesi
    xx=xtop.*zarf; %notanýn harmonikleri eklendi ve zarf ile paketlendi.
 end
-
-
-
-
-
-   
